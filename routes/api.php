c3847d39b97a28b2e3dba8c375d7ec7b3e317de7
GUDMANCHALE <chellahcharles1@gmail.com>
2023-07-18 10:03:20 +0200

    progress

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::post('/ussd', [App\Http\Controllers\UssdSessionsController::class, 'zicta']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
